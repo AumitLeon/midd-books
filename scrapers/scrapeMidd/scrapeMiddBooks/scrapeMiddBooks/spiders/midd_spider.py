40ea9f80ad45968feb3f08459645c638869a1fd7
AumitLeon <aumitleon1@gmail.com>
2017-01-17 00:00:44 -0500

    Developing web crawling framework

--- scrapers/scrapeMidd/scrapeMiddBooks/scrapeMiddBooks/spiders/midd_spider.py
+++ scrapers/scrapeMidd/scrapeMiddBooks/scrapeMiddBooks/spiders/midd_spider.py
@@ -1,4 +1,78 @@
+"""View states for asp.net"""
+
 import scrapy
+
+class BnSpider(scrapy.Spider):
+    name = 'BN'
+    start_urls = ['http://quotes.toscrape.com/search.aspx']
+    download_delay = 1.5
+
+    def parse(self, response):
+        for author in response.css('select#author > option ::attr(value)').extract():
+            yield scrapy.FormRequest(
+                'http://quotes.toscrape.com/filter.aspx',
+                formdata={
+                    'author': author,
+                    '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+                },
+                callback=self.parse_tags
+            )
+#Reverse engineer the above code to work for the middlebury book store.
+
+       """ for author in response.css('select#ctl00_ctl00_Content_Content_courseSelect_select_dept" > option ::attr(value)').extract():
+            yield scrapy.FormRequest(
+                'http://quotes.toscrape.com/filter.aspx',
+                formdata={
+                    'author': author,
+                    '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+                },
+                callback=self.parse_tags
+            )"""
+
+
+
+
+    def parse_tags(self, response):
+        for tag in response.css('select#tag > option ::attr(value)').extract():
+            yield scrapy.FormRequest(
+                'http://quotes.toscrape.com/filter.aspx',
+                formdata={
+                    'author': response.css(
+                        'select#author > option[selected] ::attr(value)'
+                    ).extract_first(),
+                    'tag': tag,
+                    '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+                },
+                callback=self.parse_results,
+            )
+
+    def parse_results(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'quote': response.css('span.content ::text').extract_first(),
+                'author': response.css('span.author ::text').extract_first(),
+                'tag': response.css('span.tag ::text').extract_first(),
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""import scrapy
 
 
 class MiddSpider(scrapy.Spider):
@@ -18,4 +92,4 @@ class MiddSpider(scrapy.Spider):
                 'text': quote.css('span.text::text').extract_first(),
                 'author': quote.css('span small::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+            }"""
